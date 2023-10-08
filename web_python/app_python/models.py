c793bdc7e24be89021e0d5c7baac96d396963fd1
KroBCoder <hinata728@gmail.com>
2023-10-08 18:00:02 -0500

    agregar modelo de usuario

--- web_python/app_python/models.py
+++ web_python/app_python/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    contraseña = models.CharField(max_length=100)  # Necesito hashear la contraseña despues
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
