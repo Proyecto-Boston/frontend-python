bfdd9b0aa222846c11e0894d61e12246d9534e62
KroBCoder <hinata728@gmail.com>
2023-10-15 23:38:57 -0500

    Middleware que verifica si el JWT es válido o no

---
+++ web_python/app_python/middleware.py
@@ -0,0 +1,47 @@
+from django.http import HttpResponseRedirect
+from zeep import Client
+from zeep.transports import Transport
+import json
+import requests
+
+class JWTAuthenticationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+        # Iniciar cliente SOAP
+        transport = Transport(session=requests.Session())
+        self.cliente = Client('http://localhost:1802/app?wsdl', transport=transport)
+        print("Conectado a WSDL")
+
+    def __call__(self, request):
+        # Vistas que no deben ejecutar el middleware
+        excluded_paths = ['/login/', '/register/', '/homepage/', '/logout/'] 
+
+        # Si la vista no está en la lista de excepciones...
+        if request.path not in excluded_paths:
+            # Obtener el JWT de la sesión actual de cookie HTTP        
+            jwt_token = request.COOKIES.get('jwt')
+
+            if jwt_token:
+                # Validar si el JWT es correcto
+                try:
+                    response = self.cliente.service.verifySession(jwt_token)
+                    if response.statusCode != 202:
+                        print("JWT inválido o vencido. Por favor vuelva a iniciar sesión")
+                        return HttpResponseRedirect('/login/')
+                    # Establecer el id del usuario que está en el sistema
+                    user_data = json.loads(response.json)
+                    request.session['user_id'] = user_data.get('user_id')
+                    print("JWT validado correctamente")
+                except Exception as e:
+                    print(f"Error verificando JWT: {str(e)}")
+                    return HttpResponseRedirect('/login/')
+            else:
+                # Si no hay JWT en las cookies
+                print("No hay ningún JWT, debe iniciar sesión")
+                return HttpResponseRedirect('/login/')
+
+        # Llamar a la siguiente vista
+        response = self.get_response(request)
+
+        return response
--- web_python/web_python/settings.py
+++ web_python/web_python/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app_python.middleware.JWTAuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'web_python.urls'
